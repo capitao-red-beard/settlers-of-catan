6570b6a8ef23eda6c5847a77b7a88abaf166efc7
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-04-06 16:08:55 +0100

    added the sea class externding terrain

---
+++ src/Sea.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Jasper on 06/04/15.
+ */
+public class Sea implements Terrain {
+
+    private String type;
+
+    public Sea() {
+        type = "Sea";
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+}
