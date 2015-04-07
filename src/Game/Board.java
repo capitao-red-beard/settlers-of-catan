99bc402d6ac08eac9a0b0b76d64519ce25636974
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-04-07 13:36:04 +0100

    added in functionality to harbours

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -1,5 +1,6 @@
 package Game;
 
+import Resources.*;
 import Terrain.*;
 
 /**
@@ -7,21 +8,28 @@ import Terrain.*;
  */
 public class Board {
 
+    private Resource resource;
+    private Brick brick;
+    private Grain grain;
+    private Lumber lumber;
+    private Ore ore;
+    private Wool wool;
+
     private Tile[] tiles;
 
     public Board() {
         tiles = new Tile[37];
 
-        tiles[0] = new Tile(new Harbor());
+        tiles[0] = new Tile(new Harbor(resource, 3));
         tiles[1] = new Tile(new Sea());
-        tiles[2] = new Tile(new Harbor());
+        tiles[2] = new Tile(new Harbor(wool, 2));
         tiles[3] = new Tile(new Sea());
         tiles[4] = new Tile(new Sea());
         tiles[5] = new Tile(new Forest(), 11);
         tiles[6] = new Tile(new Pasture(), 12);
         tiles[7] = new Tile(new Field(), 9);
-        tiles[8] = new Tile(new Harbor());
-        tiles[9] = new Tile(new Harbor());
+        tiles[8] = new Tile(new Harbor(resource, 3));
+        tiles[9] = new Tile(new Harbor(ore, 2));
         tiles[10] = new Tile(new Hill(), 4);
         tiles[11] = new Tile(new Mountain(), 6);
         tiles[12] = new Tile(new Hill(), 5);
@@ -33,8 +41,8 @@ public class Board {
         tiles[18] = new Tile(new Field(), 11);
         tiles[19] = new Tile(new Forest(), 4);
         tiles[20] = new Tile(new Field(), 8);
-        tiles[21] = new Tile(new Harbor());
-        tiles[22] = new Tile(new Harbor());
+        tiles[21] = new Tile(new Harbor(resource, 3));
+        tiles[22] = new Tile(new Harbor(grain, 2));
         tiles[23] = new Tile(new Hill(), 8);
         tiles[24] = new Tile(new Pasture(), 10);
         tiles[25] = new Tile(new Pasture(), 9);
@@ -43,11 +51,11 @@ public class Board {
         tiles[28] = new Tile(new Mountain(), 5);
         tiles[29] = new Tile(new Field(), 2);
         tiles[30] = new Tile(new Forest(), 6);
-        tiles[31] = new Tile(new Harbor());
-        tiles[32] = new Tile(new Harbor());
+        tiles[31] = new Tile(new Harbor(brick, 2));
+        tiles[32] = new Tile(new Harbor(resource, 3));
         tiles[33] = new Tile(new Sea());
         tiles[34] = new Tile(new Sea());
-        tiles[35] = new Tile(new Harbor());
+        tiles[35] = new Tile(new Harbor(lumber, 2));
         tiles[36] = new Tile(new Sea());
     }
 
--- src/Terrain/Harbor.java
+++ src/Terrain/Harbor.java
@@ -1,18 +1,28 @@
 package Terrain;
 
+import Resources.Resource;
+
 /**
  * Created by Jasper on 06/04/15.
  */
 public class Harbor implements Terrain {
 
     private String type;
+    private Resource resource;
+    private int value;
 
-    public Harbor() {
+    public Harbor(Resource resource, int value) {
         type = "Harbor";
+        this.resource = resource;
+        this.value = value;
     }
 
     @Override
     public String getType() {
         return type;
+    }
+
+    public String getTradeInfo() {
+        return "You must trade: " + value + " " + resource + " in order to get another one from the bank";
     }
 }
