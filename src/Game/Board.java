5dc372526db7622b14fcc221399ac7a812e7f20f
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-04-10 15:36:00 +0100

    quickly changed how the harbour recieves its input type

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -9,13 +9,6 @@ import Terrain.*;
  */
 public class Board {
 
-    private Resource resource;
-    private Brick brick;
-    private Grain grain;
-    private Lumber lumber;
-    private Ore ore;
-    private Wool wool;
-
     private Tile[] tiles;
     private Triple[] triples;
     private Double[] doubles;
@@ -25,16 +18,16 @@ public class Board {
         triples = new Triple[53];
         doubles = new Double[75];
 
-        tiles[0] = new Tile(new Harbor(resource, 3));
+        tiles[0] = new Tile(new Harbor(null, 3));
         tiles[1] = new Tile(new Sea());
-        tiles[2] = new Tile(new Harbor(wool, 2));
+        tiles[2] = new Tile(new Harbor(new Wool(), 2));
         tiles[3] = new Tile(new Sea());
         tiles[4] = new Tile(new Sea());
         tiles[5] = new Tile(new Forest(), 11);
         tiles[6] = new Tile(new Pasture(), 12);
         tiles[7] = new Tile(new Field(), 9);
-        tiles[8] = new Tile(new Harbor(resource, 3));
-        tiles[9] = new Tile(new Harbor(ore, 2));
+        tiles[8] = new Tile(new Harbor(null, 3));
+        tiles[9] = new Tile(new Harbor(new Ore(), 2));
         tiles[10] = new Tile(new Hill(), 4);
         tiles[11] = new Tile(new Mountain(), 6);
         tiles[12] = new Tile(new Hill(), 5);
@@ -46,8 +39,8 @@ public class Board {
         tiles[18] = new Tile(new Field(), 11);
         tiles[19] = new Tile(new Forest(), 4);
         tiles[20] = new Tile(new Field(), 8);
-        tiles[21] = new Tile(new Harbor(resource, 3));
-        tiles[22] = new Tile(new Harbor(grain, 2));
+        tiles[21] = new Tile(new Harbor(null, 3));
+        tiles[22] = new Tile(new Harbor(new Grain(), 2));
         tiles[23] = new Tile(new Hill(), 8);
         tiles[24] = new Tile(new Pasture(), 10);
         tiles[25] = new Tile(new Pasture(), 9);
@@ -56,11 +49,11 @@ public class Board {
         tiles[28] = new Tile(new Mountain(), 5);
         tiles[29] = new Tile(new Field(), 2);
         tiles[30] = new Tile(new Forest(), 6);
-        tiles[31] = new Tile(new Harbor(brick, 2));
-        tiles[32] = new Tile(new Harbor(resource, 3));
+        tiles[31] = new Tile(new Harbor(new Brick(), 2));
+        tiles[32] = new Tile(new Harbor(null, 3));
         tiles[33] = new Tile(new Sea());
         tiles[34] = new Tile(new Sea());
-        tiles[35] = new Tile(new Harbor(lumber, 2));
+        tiles[35] = new Tile(new Harbor(new Lumber(), 2));
         tiles[36] = new Tile(new Sea());
 
         tiles[0].setCenter(tiles[0]);
